4d13fb3349d66e2f53425aeb1d3d90c397fcd8f5
gonzaloguaimas <gonzaloguaimas@gmail.com>
2022-11-18 10:54:27 -0300

    add router

--- src/components/pures/NavBar.tsx
+++ src/components/pures/NavBar.tsx
@@ -2,15 +2,20 @@ import React from 'react'
 import { Menubar } from 'primereact/menubar'
 import { Button } from 'primereact/button'
 import Image from 'next/image'
+import { useRouter } from 'next/router'
 
 export const NavBar = () => {
+  const router = useRouter()
+  const navToPage = (route: any) => {
+    void router.push(route)
+  }
   const MenuItems = () => {
     return (
         <div>
-            <Button className='p-button-danger p-button-text'><Image alt={''} src={'/item-sell.png'} width={35} height={35}></Image></Button>
-            <Button className='p-button-danger p-button-text'><Image alt={''} src={'/item-orders.png'} width={35} height={35}></Image></Button>
-            <Button className='p-button-danger p-button-text'><Image alt={''} src={'/item-clothes.png'} width={35} height={35}></Image></Button>
-            <Button className='p-button-danger p-button-text'><Image alt={''} src={'/item-user.png'} width={35} height={35}></Image></Button>
+            <Button className='p-button-danger p-button-text' onClick={() => { navToPage('/orders') }}><Image alt={''} src={'/item-sell.png'} width={35} height={35}></Image></Button>
+            <Button className='p-button-danger p-button-text' onClick={() => { navToPage('/orders') }}><Image alt={''} src={'/item-orders.png'} width={35} height={35}></Image></Button>
+            <Button className='p-button-danger p-button-text' onClick={() => { navToPage('/products') }}><Image alt={''} src={'/item-clothes.png'} width={35} height={35}></Image></Button>
+            <Button className='p-button-danger p-button-text' onClick={() => { navToPage('/orders') }}><Image alt={''} src={'/item-user.png'} width={35} height={35}></Image></Button>
         </div>
     )
   }
